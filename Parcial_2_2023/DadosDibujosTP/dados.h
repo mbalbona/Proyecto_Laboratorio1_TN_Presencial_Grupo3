1320cfeb63d95005169f0f23f1977b2abe63b965
walterjp1 <135785980+walterjp1@users.noreply.github.com>
2023-06-09 20:42:44 -0300

    avance en los dados
    hasta el dado 6

--- Parcial_2_2023/DadosDibujosTP/dados.h
+++
@@ -1,7 +0,0 @@
-#ifndef DADOS_H_INCLUDED
-#define DADOS_H_INCLUDED
-
-void dibujarCuadrado(int posx ,int posy);
-void dibujarDado(int,int,int);
-void dibujarPuntos(int,int,int);
-#endif // DADOS_H_INCLUDED
