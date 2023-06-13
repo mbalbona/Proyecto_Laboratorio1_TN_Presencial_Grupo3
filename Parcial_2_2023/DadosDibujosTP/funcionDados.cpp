66ae41d9fc028f0d045a6525b7057a58227f6419
walterjp1 <135785980+walterjp1@users.noreply.github.com>
2023-06-12 19:10:40 -0600

    modifique los dados
    ashei estan del 1 al 9

--- Parcial_2_2023/DadosDibujosTP/funcionDados.cpp
+++ Parcial_2_2023/DadosDibujosTP/funcionDados.cpp
@@ -6,8 +6,8 @@ using namespace std;
     ///Funcion para dibujar
 void dibujarCuadrado(int posx,int posy){
 rlutil:: setColor(rlutil::WHITE);
-for(int x=0;x<7;x++){
-   for(int y=0;y<3;y++){
+for(int x=0;x<8;x++){
+   for(int y=0;y<4;y++){
 //llamo y uso la libreria
  rlutil::locate(x+posx, y+posy);
         cout<<(char) 219<<endl;
@@ -26,26 +26,314 @@ void dibujarPuntos(int numero,int posx,int posy){
     rlutil::setBackgroundColor(rlutil::WHITE);
 switch(numero){
 case 1:
-    rlutil::locate(posx+3, posy+1);
+    rlutil::locate(posx+0, posy+1);
     cout<<(char) 254;
+   rlutil::locate(posx+1, posy+1);
+    cout<<(char) 223;
+      rlutil::locate(posx+2, posy+0);
+    cout<<(char) 220;
+      rlutil::locate(posx+3, posy+0);
+    cout<<(char) 254;
+      rlutil::locate(posx+3, posy+1);
+    cout<<(char) 219;
+        rlutil::locate(posx+3, posy+2);
+    cout<<(char) 219;
+      rlutil::locate(posx+3, posy+3);
+    cout<<(char) 223;
+
+
     break;
 
 case 2:
 
-    rlutil::locate(posx+5, posy+1);
+    rlutil::locate(posx+0, posy+0);
     cout<<(char) 220;
+     rlutil::locate(posx+1, posy+0);
+    cout<<(char) 223;
+      rlutil::locate(posx+2, posy+0);
+    cout<<(char) 223;
+     rlutil::locate(posx+3, posy+0);
+    cout<<(char) 223;
+   rlutil::locate(posx+4, posy+0);
+    cout<<(char) 254;
+       rlutil::locate(posx+4, posy+1);
+    cout<<(char) 219;
+    rlutil::locate(posx+4, posy+2);
+    cout<<(char) 223;
+     rlutil::locate(posx+3, posy+2);
+    cout<<(char) 223;
+     rlutil::locate(posx+2, posy+2);
+    cout<<(char) 254;
+     rlutil::locate(posx+1, posy+2);
+    cout<<(char) 254;
+      rlutil::locate(posx+0, posy+2);
+    cout<<(char) 220;
+     rlutil::locate(posx+0, posy+3);
+    cout<<(char) 254;
+         rlutil::locate(posx+1, posy+3);
+    cout<<(char) 254;
+         rlutil::locate(posx+2, posy+3);
+    cout<<(char) 254;
+      rlutil::locate(posx+3, posy+3);
+    cout<<(char) 254;
+  rlutil::locate(posx+4, posy+3);
+    cout<<(char) 254;
+  rlutil::locate(posx+5, posy+3);
+    cout<<(char) 254;
+
     break;
-    rlutil::locate(posx+1,posy+2);
+    case 3:
+      rlutil::locate(posx+0, posy+0);
+    cout<<(char) 254;
+          rlutil::locate(posx+1, posy+0);
+    cout<<(char) 223;
+      rlutil::locate(posx+2, posy+0);
+    cout<<(char) 223;
+      rlutil::locate(posx+3, posy+0);
+    cout<<(char) 223;
+        rlutil::locate(posx+4, posy+0);
+    cout<<(char) 220;
+  rlutil::locate(posx+4, posy+1);
+    cout<<(char) 219;
+      rlutil::locate(posx+3, posy+1);
+    cout<<(char) 220;
+       rlutil::locate(posx+2, posy+1);
+    cout<<(char) 220;
+      rlutil::locate(posx+4, posy+2);
+    cout<<(char) 219;
+      rlutil::locate(posx+3, posy+3);
+    cout<<(char) 223;
+      rlutil::locate(posx+2, posy+3);
+    cout<<(char) 223;
+        rlutil::locate(posx+1, posy+3);
+    cout<<(char) 223;
+       rlutil::locate(posx+0, posy+3);
     cout<<(char) 223;
     break;
 
+     case 4:
+           rlutil::locate(posx+0, posy+1);
+    cout<<(char) 220;
+     rlutil::locate(posx+1, posy+1);
+    cout<<(char) 254;
+     rlutil::locate(posx+2, posy+1);
+    cout<<(char) 223;
+    rlutil::locate(posx+3, posy+0);
+    cout<<(char) 220;
+       rlutil::locate(posx+4, posy+0);
+    cout<<(char) 254;
+     rlutil::locate(posx+5, posy+0);
+    cout<<(char) 219;
+      rlutil::locate(posx+5, posy+1);
+    cout<<(char) 219;
+    rlutil::locate(posx+5, posy+2);
+    cout<<(char) 219;
+    rlutil::locate(posx+5, posy+3);
+    cout<<(char) 219;
+      rlutil::locate(posx+0, posy+2);
+    cout<<(char) 223;
+      rlutil::locate(posx+1, posy+2);
+    cout<<(char) 223;
+       rlutil::locate(posx+2, posy+2);
+    cout<<(char) 223;
+      rlutil::locate(posx+3, posy+2);
+    cout<<(char) 223;
+  rlutil::locate(posx+4, posy+2);
+    cout<<(char) 223;
+
+    break;
+     case 5:
+          rlutil::locate(posx+0, posy+0);
+    cout<<(char) 219;
+         rlutil::locate(posx+1, posy+0);
+    cout<<(char) 223;
+   rlutil::locate(posx+2, posy+0);
+    cout<<(char) 223;
+   rlutil::locate(posx+3, posy+0);
+    cout<<(char) 223;
+       rlutil::locate(posx+4, posy+0);
+    cout<<(char) 223;
+   rlutil::locate(posx+5, posy+0);
+    cout<<(char) 223;
+   rlutil::locate(posx+6, posy+0);
+    cout<<(char) 223;
+       rlutil::locate(posx+0, posy+1);
+    cout<<(char) 219;
+      rlutil::locate(posx+1, posy+1);
+    cout<<(char) 220;
+          rlutil::locate(posx+2, posy+1);
+    cout<<(char) 220;
+          rlutil::locate(posx+3, posy+1);
+    cout<<(char) 220;
+          rlutil::locate(posx+4, posy+1);
+    cout<<(char) 220;
+      rlutil::locate(posx+5, posy+1);
+    cout<<(char) 220;
+          rlutil::locate(posx+6, posy+2);
+    cout<<(char) 219;
+      rlutil::locate(posx+6, posy+3);
+    cout<<(char) 223;
+      rlutil::locate(posx+0, posy+3);
+    cout<<(char) 220;
+     rlutil::locate(posx+1, posy+3);
+    cout<<(char) 220;
+ rlutil::locate(posx+2, posy+3);
+    cout<<(char) 220;
+ rlutil::locate(posx+3, posy+3);
+    cout<<(char) 220;
+     rlutil::locate(posx+4, posy+3);
+    cout<<(char) 220;
+     rlutil::locate(posx+5, posy+3);
+    cout<<(char) 220;
+    break;
+     case 6:
+         rlutil::locate(posx+0, posy+0);
+    cout<<(char) 219;
+     rlutil::locate(posx+1, posy+0);
+    cout<<(char) 223;
+     rlutil::locate(posx+2, posy+0);
+    cout<<(char) 223;
+     rlutil::locate(posx+3, posy+0);
+    cout<<(char) 223;
+ rlutil::locate(posx+4, posy+0);
+    cout<<(char) 223;
+     rlutil::locate(posx+5, posy+0);
+    cout<<(char) 219;
+    rlutil::locate(posx+0, posy+1);
+    cout<<(char) 219;
+       rlutil::locate(posx+0, posy+2);
+    cout<<(char) 219;
+       rlutil::locate(posx+0, posy+3);
+    cout<<(char) 219;
+         rlutil::locate(posx+1, posy+3);
+    cout<<(char) 220;
+       rlutil::locate(posx+2, posy+3);
+    cout<<(char) 220;
+       rlutil::locate(posx+3, posy+3);
+    cout<<(char) 220;
+       rlutil::locate(posx+4, posy+3);
+    cout<<(char) 220;
+       rlutil::locate(posx+5, posy+3);
+    cout<<(char) 219;
+    rlutil::locate(posx+5, posy+2);
+    cout<<(char) 219;
+     rlutil::locate(posx+1, posy+2);
+    cout<<(char) 223;
+      rlutil::locate(posx+2, posy+2);
+    cout<<(char) 223;
+  rlutil::locate(posx+3, posy+2);
+    cout<<(char) 223;
+  rlutil::locate(posx+4, posy+2);
+    cout<<(char) 223;
+break;
+     case 7:
+
+    cout<<(char) 223;
+      rlutil::locate(posx+3, posy+0);
+    cout<<(char) 223;
+      rlutil::locate(posx+4, posy+0);
+    cout<<(char) 223;
+          rlutil::locate(posx+5, posy+0);
+    cout<<(char) 223;
+          rlutil::locate(posx+6, posy+0);
+    cout<<(char) 219;
+        rlutil::locate(posx+6, posy+1);
+    cout<<(char) 219;
+        rlutil::locate(posx+6, posy+2);
+    cout<<(char) 219;
+        rlutil::locate(posx+6, posy+3);
+    cout<<(char) 219;
+        rlutil::locate(posx+5, posy+2);
+    cout<<(char) 223;
+      rlutil::locate(posx+7, posy+2);
+    cout<<(char) 223;
+      rlutil::locate(posx+4, posy+2);
+    cout<<(char) 223;
+    break;
+     case 8:
+          rlutil::locate(posx+0, posy+2);
+    cout<<(char) 219;
+      rlutil::locate(posx+1, posy+1);
+    cout<<(char) 220;
+ rlutil::locate(posx+2, posy+1);
+    cout<<(char) 220;
+ rlutil::locate(posx+3, posy+1);
+    cout<<(char) 220;
+         rlutil::locate(posx+4, posy+2);
+    cout<<(char) 219;
+          rlutil::locate(posx+1, posy+3);
+    cout<<(char) 223;
+ rlutil::locate(posx+2, posy+3);
+    cout<<(char) 223;
+ rlutil::locate(posx+3, posy+3);
+    cout<<(char) 223;
+    rlutil::locate(posx+0, posy+1);
+    cout<<(char) 223;
+        rlutil::locate(posx+0, posy+0);
+    cout<<(char) 220;
+      rlutil::locate(posx+1, posy+0);
+    cout<<(char) 223;
+        rlutil::locate(posx+2, posy+0);
+    cout<<(char) 223;
+        rlutil::locate(posx+3, posy+0);
+    cout<<(char) 223;
+         rlutil::locate(posx+4, posy+1);
+    cout<<(char) 223;
+       rlutil::locate(posx+4, posy+0);
+    cout<<(char) 220;
+    break;
+     case 9:
+                 rlutil::locate(posx+0, posy+1);
+    cout<<(char) 219;
+     rlutil::locate(posx+0, posy+0);
+    cout<<(char) 219;
+         rlutil::locate(posx+1,posy+0);
+    cout<<(char) 223;
+       rlutil::locate(posx+2,posy+0);
+    cout<<(char) 223;
+   rlutil::locate(posx+3,posy+0);
+    cout<<(char) 223;
+  rlutil::locate(posx+4,posy+0);
+    cout<<(char) 223;
+
+  rlutil::locate(posx+5,posy+0);
+    cout<<(char) 223;
+         rlutil::locate(posx+6,posy+0);
+    cout<<(char) 219;
+     rlutil::locate(posx+6,posy+1);
+    cout<<(char) 219;
+ rlutil::locate(posx+6,posy+2);
+    cout<<(char) 219;
+     rlutil::locate(posx+6,posy+3);
+    cout<<(char) 219;
+           rlutil::locate(posx+1,posy+1);
+    cout<<(char) 220;
+      rlutil::locate(posx+2,posy+1);
+    cout<<(char) 220;
+      rlutil::locate(posx+3,posy+1);
+    cout<<(char) 220;
+ rlutil::locate(posx+4,posy+1);
+    cout<<(char) 220;
+ rlutil::locate(posx+5,posy+1);
+    cout<<(char) 220;
+       rlutil::locate(posx+5,posy+3);
+    cout<<(char) 220;
+
+   rlutil::locate(posx+4,posy+3);
+    cout<<(char) 220;
+   rlutil::locate(posx+3,posy+3);
+    cout<<(char) 220;
+   rlutil::locate(posx+2,posy+3);
+    cout<<(char) 220;
+   rlutil::locate(posx+1,posy+3);
+    cout<<(char) 220;
+
+  rlutil::locate(posx+0,posy+3);
+    cout<<(char) 219;
+
+
 }
 
-
-
-
 }
-
-
 
 
--- Parcial_2_2023/DadosDibujosTP/main.cpp
+++ Parcial_2_2023/DadosDibujosTP/main.cpp
@@ -13,6 +13,10 @@ dibujarDado(3,20,5);
 dibujarDado(4,30,5);
 dibujarDado(5,40,5);
 dibujarDado(6,50,5);
+dibujarDado(7,60,5);
+dibujarDado(8,70,5);
+dibujarDado(9,80,5);
+
 
 //muevo el return para que se veo mejor el dado.
 rlutil::locate(1,24);
